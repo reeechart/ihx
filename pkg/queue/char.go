efb93f731a17ed7a78b561b72bee1f1cd9ad0fd4
Ferdinandus Richard <reeechart@gmail.com>
2021-08-18 16:33:39 +0700

    Implement queue stdlib

---
+++ pkg/queue/char.go
@@ -0,0 +1,54 @@
+package queue
+
+import "errors"
+
+var (
+	ErrQueueEmpty = errors.New("queue is empty")
+)
+
+// CharQueue is a queue that holds a collection of chars.
+type CharQueue struct {
+	queue []string
+}
+
+// NewCharFromString creates a new CharQueue from a string.
+// The queue is intended to receive only alphanumeric characters.
+// The elements of the queue will be individual characters contained in the string.
+func NewCharFromString(s string) *CharQueue {
+	queue := make([]string, len(s))
+	for i := range s {
+		queue[i] = string(s[i])
+	}
+
+	return &CharQueue{queue}
+}
+
+func (q *CharQueue) IsEmpty() bool {
+	return len(q.queue) == 0
+}
+
+// Peek returns the first element of the queue, this operation does not change the state of the queue.
+func (q *CharQueue) Peek() (string, error) {
+	if q.IsEmpty() {
+		return "", ErrQueueEmpty
+	}
+
+	return q.queue[0], nil
+}
+
+// Enqueue adds new element to the end of queue, element is assumed to be a single character in form of string.
+func (q *CharQueue) Enqueue(char string) {
+	q.queue = append(q.queue, char)
+}
+
+// Dequeue removes and returns the first element of the queue.
+func (q *CharQueue) Dequeue() (string, error) {
+	if q.IsEmpty() {
+		return "", ErrQueueEmpty
+	}
+
+	char := q.queue[0]
+	q.queue = q.queue[1:]
+
+	return char, nil
+}
