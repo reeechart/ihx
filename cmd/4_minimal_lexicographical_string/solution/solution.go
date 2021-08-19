b7d98f66c81fc68919ecf44e189824fa576ea6c1
Ferdinandus Richard <reeechart@gmail.com>
2021-08-19 14:26:29 +0700

    Implement edge case handling to the fourth problem solution (minimal lexicographical string)

--- cmd/4_minimal_lexicographical_string/solution/solution.go
+++ cmd/4_minimal_lexicographical_string/solution/solution.go
@@ -27,9 +27,13 @@ func GenerateLexicographicallyMinimalString(a string, b string) string {
 			result = fmt.Sprintf("%s%s", result, charA)
 			aq.Dequeue()
 		default: // case 0
-			// TODO: Decide which side to pick by peeking the next element, if still matches, pick any side
-			result = fmt.Sprintf("%s%s", result, charA)
-			aq.Dequeue()
+			if pickNext(aq, bq) == 1 {
+				result = fmt.Sprintf("%s%s", result, charA)
+				aq.Dequeue()
+			} else {
+				result = fmt.Sprintf("%s%s", result, charB)
+				bq.Dequeue()
+			}
 		}
 	}
 
@@ -44,4 +48,57 @@ func GenerateLexicographicallyMinimalString(a string, b string) string {
 	}
 
 	return result
+}
+
+// PickNext returns either 1 or -1 by comparing two of the queues.
+// Queue size needs to be guaranteed to be greater than 0, otherwise it will panic.
+// Returns 1 when aq needs to be picked next.
+// Returns -1 when bq needs to be picked next.
+func pickNext(aq *queue.CharQueue, bq *queue.CharQueue) int {
+	i := 0
+
+	for i < aq.Size() && i < bq.Size() {
+		a, _ := aq.Get(i)
+		b, _ := bq.Get(i)
+
+		switch strings.Compare(a, b) {
+		case 1:
+			return -1
+		case -1:
+			return 1
+		default: // case 0
+			i += 1
+		}
+	}
+
+	for i < aq.Size() {
+		a, _ := aq.Get(i)
+		b, _ := bq.Get(0)
+
+		switch strings.Compare(a, b) {
+		case 1:
+			return -1
+		case -1:
+			return 1
+		default: // case 0
+			i += 1
+		}
+	}
+
+	for i < bq.Size() {
+		a, _ := aq.Get(0)
+		b, _ := bq.Get(i)
+
+		switch strings.Compare(a, b) {
+		case 1:
+			return -1
+		case -1:
+			return 1
+		default: // case 0
+			i += 1
+		}
+	}
+
+	// All characters are equal, return anything
+	return 1
 }
